cd68f2e300a46709cb8046de8b6d67db9db5e857
aot29 <alvaro@zbmath.org>
2022-02-17 10:08:43 +0100

    Clone from Gerrit by default, fallback on Github

--- clone-extension.sh
+++ clone-extension.sh
@@ -2,5 +2,7 @@
 EXTENSION=$1
 BRANCH=$2
 
-git clone https://github.com/wikimedia/mediawiki-extensions-${EXTENSION}.git -b ${BRANCH} ${EXTENSION}
+# Clone from gerrit by default. If it doesn't work, clone from github.
+git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/$EXTENSiON -b ${BRANCH} ${EXTENSION} \
+|| git clone https://github.com/wikimedia/mediawiki-extensions-${EXTENSION}.git -b ${BRANCH} ${EXTENSION}
 rm -rf ${EXTENSION}/.git
