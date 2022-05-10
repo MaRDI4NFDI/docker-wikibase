8cdeb3470eca1f50e8ffd531502726f4c594fdf5
Stegmujo <johannesst@gmx.de>
2022-05-10 18:59:27 +0200

    Regular_maintenance script is updating imported pages every hour.

---
+++ regular_maintenance.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Refreshing the content of external pages imported pages.
+# Only use this for maintenance scripts which do not take much time.
+echo "starting regular_maintenance at:"`date +"%Y-%m-%d %T"`
+php /var/www/html/extensions/ExternalContent/maintenance/RefreshExternalContent.php
+echo "done with regular_maintenance at:"`date +"%Y-%m-%d %T"`
+
+
