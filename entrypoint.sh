973e9787f2c005e05480b5e4c4cd3e9de0ae5bae
dajuno <dajuno@riseup.net>
2022-05-11 11:37:52 +0200

    always recreate localsettings.php from template

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,11 +25,6 @@ if [ -f /extra-entrypoint-run-first.sh ]; then
     source /extra-entrypoint-run-first.sh
 fi
 
-# Copy LocalSettings from share if exists
-if [ -e "/shared/LocalSettings.php" ]; then
-  cp /shared/LocalSettings.php /var/www/html/LocalSettings.php
-fi
-
 # Do the mediawiki install (only if LocalSettings doesn't already exist)
 if [ ! -e "/var/www/html/LocalSettings.php" ]; then
     php /var/www/html/maintenance/install.php --dbuser "$DB_USER" --dbpass "$DB_PASS" --dbname "$DB_NAME" --dbserver "$DB_SERVER" --lang "$MW_SITE_LANG" --pass "$MW_ADMIN_PASS" "$MW_SITE_NAME" "$MW_ADMIN_NAME"
@@ -49,11 +44,13 @@ if [ ! -e "/var/www/html/LocalSettings.php" ]; then
     if [ -f /extra-install.sh ]; then
         source /extra-install.sh
     fi
-fi
 
-# Copy LocalSettings to shared location
-if [ ! -e "/shared/LocalSettings.php" ]; then
-  cp /var/www/html/LocalSettings.php /shared/LocalSettings.php
+else
+    # LocalSettings.php exists: do not install wiki, just update LocalSettings from template
+    export DOLLAR='$'
+    envsubst < /LocalSettings.php.template > /var/www/html/LocalSettings.php
+    # Copy LocalSettings to shared location
+    cp /var/www/html/LocalSettings.php /shared/LocalSettings.php
 fi
 
 # Run the actual entry point
