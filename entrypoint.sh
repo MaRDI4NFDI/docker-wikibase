74506942e14a5bfcc273978b23ebaeb866c79a41
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-05-13 18:15:46 +0200

    Only require env_variables when needed
    Close #34

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 # This file is provided by the wikibase/wikibase docker image.
 
-# Test if required environment variables have been set
-REQUIRED_VARIABLES=(MW_ADMIN_NAME MW_ADMIN_PASS MW_ADMIN_EMAIL MW_WG_SECRET_KEY DB_SERVER DB_USER DB_PASS DB_NAME)
-for i in ${REQUIRED_VARIABLES[@]}; do
-    eval THISSHOULDBESET=\$$i
-    if [ -z "$THISSHOULDBESET" ]; then
-    echo "$i is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
-    exit 1;
-    fi
-done
+
 
 set -eu
 
-# Wait for the db to come up
-/wait-for-it.sh $DB_SERVER -t 300
-# Sometimes it appears to come up and then go back down meaning MW install fails
-# So wait for a second and double check!
-sleep 1
-/wait-for-it.sh $DB_SERVER -t 300
+if [[ -z "${DB_SERVER}" ]]; then
+  # Wait for the db to come up
+  /wait-for-it.sh $DB_SERVER -t 300
+  # Sometimes it appears to come up and then go back down meaning MW install fails
+  # So wait for a second and double check!
+  sleep 1
+  /wait-for-it.sh $DB_SERVER -t 300
+fi
 
 # Run extra scripts everytime
 if [ -f /extra-entrypoint-run-first.sh ]; then
@@ -32,6 +26,15 @@ fi
 
 # Do the mediawiki install (only if LocalSettings doesn't already exist)
 if [ ! -e "/var/www/html/LocalSettings.php" ]; then
+    # Test if required environment variables have been set
+    REQUIRED_VARIABLES=(MW_ADMIN_NAME MW_ADMIN_PASS MW_ADMIN_EMAIL MW_WG_SECRET_KEY DB_SERVER DB_USER DB_PASS DB_NAME)
+    for i in ${REQUIRED_VARIABLES[@]}; do
+        eval THISSHOULDBESET=\$$i
+        if [ -z "$THISSHOULDBESET" ]; then
+        echo "$i is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
+        exit 1;
+        fi
+    done
     php /var/www/html/maintenance/install.php --dbuser "$DB_USER" --dbpass "$DB_PASS" --dbname "$DB_NAME" --dbserver "$DB_SERVER" --lang "$MW_SITE_LANG" --pass "$MW_ADMIN_PASS" "$MW_SITE_NAME" "$MW_ADMIN_NAME"
     php /var/www/html/maintenance/resetUserEmail.php --no-reset-password "$MW_ADMIN_NAME" "$MW_ADMIN_EMAIL"
 
--- extra-entrypoint-run-first.sh
+++ extra-entrypoint-run-first.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
-/wait-for-it.sh $MW_ELASTIC_HOST:$MW_ELASTIC_PORT -t 300
+if [[ -z "${MW_ELASTIC_HOST}" ]]; then
+  /wait-for-it.sh $MW_ELASTIC_HOST:$MW_ELASTIC_PORT -t 300
+fi
