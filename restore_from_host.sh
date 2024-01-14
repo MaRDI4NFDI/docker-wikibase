35a8caed34f9d2c232516018afa32da47d097107
Moritz Schubotz <github@ckurs.de>
2024-01-14 10:59:36 +0100

    Create restore_from_host.sh

---
+++ restore_from_host.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# testfile moritz
+FILENAME=./portal_db_backup_2024.01.12_04.15.01.gz
+echo "Starting restoring to $FILENAME"
+gunzip -c $FILENAME | docker exec -it mardi-mysql sh -c 'exec mysql $MYSQL_DATABASE -u $MYSQL_USER -p$MYSQL_PASSWORD'
+echo "Restore done"
