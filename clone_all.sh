12149cd328ea794d3c60f0ac35feb5d7c782ad13
physikerwelt <wiki@physikerwelt.de>
2025-03-31 07:17:03 +0200

    Inherit fixes from ProfessionalWikiMake clone script fail on error

--- clone_all.sh
+++ clone_all.sh
@@ -22,7 +22,7 @@ EXTENSIONS=(
   "Echo ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Echo.git"
   "Elastica ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Elastica.git"
   "EntitySchema ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-EntitySchema.git"
-  # Wokraround for https://phabricator.wikimedia.org/T388624
+  "ExternalContent master https://github.com/ProfessionalWiki/ExternalContent.git"
   "ExternalData master https://github.com/wikimedia/mediawiki-extensions-ExternalData.git"
   "Gadgets ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Gadgets.git"
   "Graph ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Graph.git"
@@ -32,6 +32,7 @@ EXTENSIONS=(
   "Lockdown ${REL_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Lockdown.git"
   "Math master https://github.com/wikimedia/mediawiki-extensions-Math.git"
   "MathSearch master https://github.com/wikimedia/mediawiki-extensions-MathSearch.git"
+  "MatomoAnalytics master https://github.com/MaRDI4NFDI/MatomoAnalytics.git"
   "MultimediaViewer ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-MultimediaViewer.git"
   "Nuke ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Nuke.git"
   "OAuth ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-OAuth.git"
@@ -40,11 +41,13 @@ EXTENSIONS=(
   "PageImages ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-PageImages.git"
   "ParserFunctions ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-ParserFunctions.git"
   "PdfHandler ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-PdfHandler.git"
-  # Workaround for https://phabricator.wikimedia.org/T388624
   "PluggableAuth master https://github.com/wikimedia/mediawiki-extensions-PluggableAuth.git"
   "Popups ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Popups.git"
   "ReplaceText ${REL_BRANCH} https://github.com/wikimedia/mediawiki-extensions-ReplaceText.git"
   "Scribunto ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Scribunto.git"
+  "SemanticDrilldown master https://github.com/MaRDI4NFDI/SemanticDrilldown.git"
+  "SemanticMediaWiki master https://github.com/SemanticMediaWiki/SemanticMediaWiki.git"
+  "SPARQL master https://github.com/ProfessionalWiki/SPARQL.git"
   "SyntaxHighlight_GeSHi ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-SyntaxHighlight_GeSHi.git"
   "TemplateStyles ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-TemplateStyles.git"
   "TextExtracts ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-TextExtracts.git"
@@ -56,20 +59,14 @@ EXTENSIONS=(
   "VisualEditor ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-VisualEditor.git"
   "Widgets ${REL_BRANCH} https://github.com/wikimedia/mediawiki-extensions-Widgets.git"
   "WikibaseCirrusSearch ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-WikibaseCirrusSearch.git"
+  "WikibaseExport master https://github.com/ProfessionalWiki/WikibaseExport.git"
+  "WikibaseFacetedSearch master https://github.com/ProfessionalWiki/WikibaseFacetedSearch.git"
   "WikibaseLexeme ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-WikibaseLexeme.git"
+  "WikibaseLocalMedia master https://github.com/ProfessionalWiki/WikibaseLocalMedia.git"
   "WikibaseManifest ${REL_BRANCH} https://github.com/wikimedia/mediawiki-extensions-WikibaseManifest.git"
   "WikibaseQualityConstraints ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-WikibaseQualityConstraints.git"
   "WikiEditor ${WMF_BRANCH} https://github.com/wikimedia/mediawiki-extensions-WikiEditor.git"
   "YouTube ${REL_BRANCH} https://github.com/wikimedia/mediawiki-extensions-YouTube.git"
-  # Additional repositories (integrated into the main loop)
-  "WikibaseLocalMedia master https://github.com/MaRDI4NFDI/WikibaseLocalMedia.git"
-  "WikibaseExport master https://github.com/ProfessionalWiki/WikibaseExport.git"
-  "ExternalContent master https://github.com/ProfessionalWiki/ExternalContent.git"
-  "SPARQL master https://github.com/ProfessionalWiki/SPARQL.git"
-  "WikibaseFacetedSearch master https://github.com/ProfessionalWiki/WikibaseFacetedSearch.git"
-  "MatomoAnalytics master https://github.com/MaRDI4NFDI/MatomoAnalytics.git"
-  "SemanticMediaWiki master https://github.com/SemanticMediaWiki/SemanticMediaWiki.git"
-  "SemanticDrilldown master https://github.com/MaRDI4NFDI/SemanticDrilldown.git"
 )
 
 for ext in "${EXTENSIONS[@]}"
@@ -104,6 +101,5 @@ cd ../../..
 # Clone core and other skins
 git clone --depth=1 https://github.com/wikimedia/mediawiki-skins-Vector -b ${WMF_BRANCH} mediawiki/skins/Vector
   # Other skins
-# see https://github.com/ProfessionalWiki/chameleon/pull/462  
-git clone --depth=1 https://github.com/physikerwelt/chameleon.git -b patch-1 mediawiki/skins/chameleon 
+git clone --depth=1 https://github.com/ProfessionalWiki/chameleon.git -b patch-1 mediawiki/skins/chameleon 
 git clone --depth=1 https://github.com/ProfessionalWiki/MardiSkin.git mediawiki/skins/MardiSkin
