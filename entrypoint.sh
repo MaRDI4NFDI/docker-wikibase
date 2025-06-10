fc886cb5402a91e40a9bdc5db8cc70089f73f72e
Eloi Ferrer <ferrer@zib.de>
2025-06-10 13:17:45 +0000

    comment out ve npm start

--- entrypoint.sh
+++ entrypoint.sh
@@ -66,5 +66,5 @@ fi
 /etc/init.d/cron start
 
 # Run the actual entry point
-(cd /var/www/html/w/extensions/VisualEditor/lib/ve/rebaser;npm start) &
+# (cd /var/www/html/w/extensions/VisualEditor/lib/ve/rebaser;npm start) &
 docker-php-entrypoint apache2-foreground
