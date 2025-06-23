8c951423964014aebc6d5e65261bd15adbd4738d
Eloi Ferrer <ferrer@zib.de>
2025-06-23 11:06:12 +0000

    try new S3 setting

--- LocalSettings.d/base/AWS.php
+++ LocalSettings.d/base/AWS.php
@@ -17,7 +17,7 @@ $wgFileBackends['s3']['use_path_style_endpoint'] = true;
 
 $wgAWSRegion = 'default';
 $wgAWSBucketName = 'mardi-portal';
-$wgAWSBucketDomain = $s3endpoint . '/' . $wgAWSBucketName;
+$wgAWSBucketDomain = $s3endpoint . '/$1';
 $wgAWSBucketTopSubdirectory = "/" . getenv('S3_ENVIRONMENT');
 $wgAWSRepoHashLevels = '2';
 $wgAWSRepoDeletedHashLevels = '3';
