29b78cbebc7c30e28757ff4aab6f82e4718f6820
Eloi Ferrer <ferrer@zib.de>
2025-06-23 10:34:47 +0000

    fix bucket domain definition

--- LocalSettings.d/base/AWS.php
+++ LocalSettings.d/base/AWS.php
@@ -13,10 +13,11 @@ $wgFileBackends['s3']['endpoint'] = 'https://' . $s3endpoint;
 $wgFileBackends['s3']['use_path_style_endpoint'] = true; 
 
 // $wgAWSBucketDomain = '$1.' . $s3endpoint;
-$wgAWSBucketDomain = $s3endpoint;
+// $wgAWSBucketDomain = $s3endpoint;
 
 $wgAWSRegion = 'default';
 $wgAWSBucketName = 'mardi-portal';
+$wgAWSBucketDomain = $s3Endpoint . '/' . $wgAWSBucketName;
 $wgAWSBucketTopSubdirectory = "/" . getenv('S3_ENVIRONMENT');
 $wgAWSRepoHashLevels = '2';
 $wgAWSRepoDeletedHashLevels = '3';
