cf3ff8080b014e7b195fece91f7e211cfbdb7f3f
Eloi Ferrer <ferrer@zib.de>
2025-09-24 11:14:01 +0000

    conditionally activate OpenIDConnect

--- LocalSettings.d/base/OpenID_Connect.php
+++ LocalSettings.d/base/OpenID_Connect.php
@@ -1,12 +1,14 @@
 <?php
-wfLoadExtension( 'OpenIDConnect' );
-$wgPluggableAuth_Config['Login with your NFDI AAI Account'] = [
-    'plugin' => 'OpenIDConnect',
-    'data' => [
-        'providerURL' => 'https://auth.didmos.nfdi-aai.de',
-        'clientID' => $_ENV['NFDI_CLIENT_ID'],
-        'clientsecret' => $_ENV['NFDI_AAI_SECRET'],
-        'scope' => [],
-    ]
-];
-$wgOpenIDConnect_MigrateUsersByEmail=true;
+if (getenv('NFDI_CLIENT_ID') && getenv('NFDI_AAI_SECRET')) {
+    wfLoadExtension( 'OpenIDConnect' );
+    $wgPluggableAuth_Config['Login with your NFDI AAI Account'] = [
+        'plugin' => 'OpenIDConnect',
+        'data' => [
+            'providerURL' => 'https://auth.didmos.nfdi-aai.de',
+            'clientID' => $_ENV['NFDI_CLIENT_ID'],
+            'clientsecret' => $_ENV['NFDI_AAI_SECRET'],
+            'scope' => [],
+        ]
+    ];
+    $wgOpenIDConnect_MigrateUsersByEmail=true;
+}
