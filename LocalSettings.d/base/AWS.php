efebc74c72cc30a6cbc9ff3ec20d8ef2bc8d5407
Eloi Ferrer <ferrer@zib.de>
2025-09-24 10:56:33 +0000

    conditionally activate AWS extension

--- LocalSettings.d/base/AWS.php
+++ LocalSettings.d/base/AWS.php
@@ -1,26 +1,28 @@
 <?php
 
-wfLoadExtension( 'AWS' );
+if (getenv('S3_IMAGES_KEY') && getenv('S3_IMAGES_SECRET') && getenv('S3_ENVIRONMENT')) {
+    wfLoadExtension( 'AWS' );
 
-$wgAWSCredentials = [
-	'key' => getenv('S3_IMAGES_KEY'),
-	'secret' => getenv('S3_IMAGES_SECRET'),
-	'token' => false
-];
+    $wgAWSCredentials = [
+        'key' => getenv('S3_IMAGES_KEY'),
+        'secret' => getenv('S3_IMAGES_SECRET'),
+        'token' => false
+    ];
 
-$s3endpoint = getenv('S3_ENDPOINT');
-$wgFileBackends['s3']['endpoint'] = 'https://' . $s3endpoint;
-$wgFileBackends['s3']['use_path_style_endpoint'] = true; 
+    $s3endpoint = getenv('S3_ENDPOINT');
+    $wgFileBackends['s3']['endpoint'] = 'https://' . $s3endpoint;
+    $wgFileBackends['s3']['use_path_style_endpoint'] = true; 
 
-// $wgAWSBucketDomain = '$1.' . $s3endpoint;
-// $wgAWSBucketDomain = $s3endpoint;
+    // $wgAWSBucketDomain = '$1.' . $s3endpoint;
+    // $wgAWSBucketDomain = $s3endpoint;
 
-$wgAWSRegion = 'default';
-$wgAWSBucketName = 'mardi-portal';
+    $wgAWSRegion = 'default';
+    $wgAWSBucketName = 'mardi-portal';
 
-//$wgAWSBucketDomain = $s3endpoint . '/$1';
-$wgAWSBucketDomain = 'images.' . getenv('WIKIBASE_HOST');
+    //$wgAWSBucketDomain = $s3endpoint . '/$1';
+    $wgAWSBucketDomain = 'images.' . getenv('WIKIBASE_HOST');
 
-$wgAWSBucketTopSubdirectory = "/" . getenv('S3_ENVIRONMENT');
-$wgAWSRepoHashLevels = '2';
-$wgAWSRepoDeletedHashLevels = '3';
+    $wgAWSBucketTopSubdirectory = "/" . getenv('S3_ENVIRONMENT');
+    $wgAWSRepoHashLevels = '2';
+    $wgAWSRepoDeletedHashLevels = '3';
+}
