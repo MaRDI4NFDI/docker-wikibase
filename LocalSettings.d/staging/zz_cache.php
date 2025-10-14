5268964f9b259b072f0eb15de609067790d5e250
Eloi Ferrer <ferrer@zib.de>
2025-10-14 08:05:54 +0000

    deactivate file cache in staging

--- LocalSettings.d/staging/zz_cache.php
+++ LocalSettings.d/staging/zz_cache.php
@@ -4,3 +4,4 @@ $wgMainCacheType = CACHE_MEMCACHED;
 $wgMemCachedServers = [ 
     (getenv('MW_MEMCACHED_HOST') ?: 'memcached') . ':' . (getenv('MW_MEMCACHED_PORT') ?: '11211')
 ];
+$wgUseFileCache=false;
