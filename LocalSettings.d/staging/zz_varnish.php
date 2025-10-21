9f2533e457272a67778c26f89c780c5f9eec057d
Eloi Ferrer <ferrer@zib.de>
2025-10-21 09:52:25 +0000

    test purge restriction

--- LocalSettings.d/staging/zz_varnish.php
+++ LocalSettings.d/staging/zz_varnish.php
@@ -6,3 +6,5 @@ $wgInternalServer = str_replace('https://', 'http://', $wgServer);
 $wgCdnMaxAge = 18000; // 5 hours (default)
 $wgCdnMaxageLagged = 18000; // MediaWiki incorrectly detects lagged database due to Galera cluster architecture.
 $wgUseFileCache=false;
+
+$wgGroupPermissions['*']['purge'] = false;
