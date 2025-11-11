8f749038def93b5b51d7fd6f691bd8a32ca57b32
Eloi Ferrer <ferrer@zib.de>
2025-11-11 09:27:49 +0000

    pass cdn internalserver

--- LocalSettings.d/base/cdn.php
+++ LocalSettings.d/base/cdn.php
@@ -4,7 +4,7 @@ $wgUseCdn = getenv('USE_CDN') === 'true' ? true : false;
 
 if ($wgUseCdn) {
     $wgCdnServers = [ '10.217.0.0/16' ];
-    $wgInternalServer = str_replace('https://', 'http://', $wgServer);
+    $wgInternalServer = getenv('CDN_BACKEND_HOST');
     $wgCdnMaxAge = 18000; // 5 hours (default)
     $wgCdnMaxageLagged = 18000; // MediaWiki incorrectly detects lagged database due to Galera cluster architecture.
     $wgUsePrivateIPs = true; // Trust proxy headers from Varnish
