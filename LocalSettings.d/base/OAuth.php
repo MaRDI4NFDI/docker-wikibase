b584e6072493b9b6c6f584496c636b4b19405232
Eloi Ferrer <ferrer@zib.de>
2025-11-11 12:50:08 +0000

    fix OAuth deprecation warning

--- LocalSettings.d/base/OAuth.php
+++ LocalSettings.d/base/OAuth.php
@@ -9,3 +9,7 @@ $wgGroupPermissions['sysop']['mwoauthupdateownconsumer'] = true;
 
 $wgOAuth2PrivateKey = "/var/oauth/oauth.key";
 $wgOAuth2PublicKey = "/var/oauth/oauth.cert";
+
+# Fix the deprecation warning:
+$wgMWOAuthSharedUserIDs = true;
+$wgMWOAuthSharedUserSource = 'local';
