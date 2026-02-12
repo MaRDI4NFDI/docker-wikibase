b8d424c1b926e659da344ba9a5d01b88a829997b
Moritz Schubotz <wiki@physikerwelt.de>
2026-02-12 14:54:36 +0100

    Set wgPluggableAuth_EnableLocalProperties to true
    Enable local properties for PluggableAuth extension.
    
    cf https://github.com/MaRDI4NFDI/MaRDIRoadmap/issues/160#issuecomment-3890934841

--- LocalSettings.d/base/PluggableAuth.php
+++ LocalSettings.d/base/PluggableAuth.php
@@ -1,5 +1,7 @@
 <?php
 # https://www.mediawiki.org/wiki/Extension:PluggableAuth
-
+// If people do it things might break. From the docs:
+// If true, users can edit their email address and real name on the wiki. If false, the default, they cannot do so. Note that, if you rely on email address and/or real name returned from the authentication provider in any way, you should leave this setting at its default value.
+$wgPluggableAuth_EnableLocalProperties=true;
 $wgPluggableAuth_EnableLocalLogin=true;
 wfLoadExtension( 'PluggableAuth' );
