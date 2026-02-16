a5c16e1cbec6ed71e2d38e20ed188c07df5c7bde
Eloi Ferrer <ferrer@zib.de>
2026-02-16 11:53:42 +0000

    define baseUri with http in staging

---
+++ LocalSettings.d/staging/zz_overwrite_baseUri.php
@@ -0,0 +1,17 @@
+<?php
+
+$wgWBClientSettings['entitySources'] = [
+		'mardi_source' => [
+				'repoDatabase' => 'my_wiki',
+				'baseUri' => 'http://'. getenv( 'WIKIBASE_HOST' ) . '/entity/',
+				'entityNamespaces' => [
+						'item' => 120,
+						'property' => 122,
+				],
+				'rdfNodeNamespacePrefix' => 'wd',
+				'rdfPredicateNamespacePrefix' => '',
+				'interwikiPrefix' => '',
+		],
+];
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
