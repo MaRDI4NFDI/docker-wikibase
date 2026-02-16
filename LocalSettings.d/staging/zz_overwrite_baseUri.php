7ddae83bca99269346d84641914e1351ff911c7e
Eloi Ferrer <ferrer@zib.de>
2026-02-16 12:07:48 +0000

    overwrite http baseUri in RepoSettings for staging

--- LocalSettings.d/staging/zz_overwrite_baseUri.php
+++ LocalSettings.d/staging/zz_overwrite_baseUri.php
@@ -1,17 +1,32 @@
 <?php
 
 $wgWBClientSettings['entitySources'] = [
-		'mardi_source' => [
-				'repoDatabase' => 'my_wiki',
-				'baseUri' => 'http://'. getenv( 'WIKIBASE_HOST' ) . '/entity/',
-				'entityNamespaces' => [
-						'item' => 120,
-						'property' => 122,
-				],
-				'rdfNodeNamespacePrefix' => 'wd',
-				'rdfPredicateNamespacePrefix' => '',
-				'interwikiPrefix' => '',
-		],
+    'mardi_source' => [
+        'repoDatabase' => 'my_wiki',
+        'baseUri' => 'http://'. getenv( 'WIKIBASE_HOST' ) . '/entity/',
+        'entityNamespaces' => [
+            'item' => 120,
+            'property' => 122,
+        ],
+        'rdfNodeNamespacePrefix' => 'wd',
+        'rdfPredicateNamespacePrefix' => '',
+        'interwikiPrefix' => '',
+    ],
 ];
+
+$wgWBRepoSettings['entitySources'] = [
+      'mardi_source' => [
+        'repoDatabase' => 'my_wiki',
+        'baseUri' => 'http://'. getenv( 'WIKIBASE_HOST' ) . '/entity/',
+        'entityNamespaces' => [
+            'item' => 120,
+            'property' => 122,
+        ],
+        'rdfNodeNamespacePrefix' => 'wd',
+        'rdfPredicateNamespacePrefix' => '',
+        'interwikiPrefix' => '',
+      ],
+
+  ];
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
