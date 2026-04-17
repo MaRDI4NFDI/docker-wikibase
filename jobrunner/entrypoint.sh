99055a0b82b42200df17be1474aab6df7e7e2d9e
Eloi Ferrer <ferrer@zib.de>
2026-04-17 09:49:53 +0000

    remove obsolete jobrunner

--- jobrunner/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Do the environment substitution from the original image
-export DOLLAR='$'
-envsubst < /LocalSettings.php.template > /var/www/html/w/LocalSettings.php
-
-exec "$@"
