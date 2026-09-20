0b9c65caf464f43a783307c2fc69fc0cf649ddee
physikerwelt <wiki@physikerwelt.de>
2026-09-20 09:14:38 +0200

    Expose Apache backpressure controls and metrics

--- apache/entrypoint.sh
+++ apache/entrypoint.sh
@@ -14,8 +14,21 @@ case "${APACHE_MAX_REQUEST_WORKERS:-}" in
         ;;
 esac
 
-export APACHE_MAX_REQUEST_WORKERS_DIRECTIVE
-envsubst '${WIKIBASE_HOST} ${APACHE_MAX_REQUEST_WORKERS_DIRECTIVE}' \
+case "${APACHE_LISTEN_BACKLOG:-}" in
+    "")
+        APACHE_LISTEN_BACKLOG_DIRECTIVE=""
+        ;;
+    *[!0-9]*|0)
+        echo "APACHE_LISTEN_BACKLOG must be a positive integer" >&2
+        exit 1
+        ;;
+    *)
+        APACHE_LISTEN_BACKLOG_DIRECTIVE="ListenBacklog ${APACHE_LISTEN_BACKLOG}"
+        ;;
+esac
+
+export APACHE_MAX_REQUEST_WORKERS_DIRECTIVE APACHE_LISTEN_BACKLOG_DIRECTIVE
+envsubst '${WIKIBASE_HOST} ${APACHE_MAX_REQUEST_WORKERS_DIRECTIVE} ${APACHE_LISTEN_BACKLOG_DIRECTIVE}' \
     < /usr/local/apache2/conf/extra/mediawiki.conf.template \
     > /usr/local/apache2/conf/extra/mediawiki.conf
 
